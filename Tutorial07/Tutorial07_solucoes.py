f71b5b4d6d7194d9074526cecedd011a958ae7e7
Fabio Casagrande Hirono <fchirono@gmail.com>
2026-08-12 19:13:47 -0300

    Renomeia variaveis, zera valores pequenos

--- Tutorial07/Tutorial07_solucoes.py
+++ Tutorial07/Tutorial07_solucoes.py
@@ -51,82 +51,82 @@ t = np.linspace(0, T-dt, Nt)        # vetor de amostras no tempo
 
 # %% funcoes para obter coeficientes de Fourier 
 
-def coef_dente_sen(m):
+def coef_dente_sen(k):
     # Onda dente-de-serra (serie de Fourier de senos)
-    return -2*((-1)**(m+1))/(np.pi*m)
+    return -2*((-1)**(k+1))/(np.pi*k)
 
-def coef_quad_sen(m):
+def coef_quad_sen(k):
     # onda quadrada (serie de Fourier de senos)
-    return 2*(1-np.cos(np.pi*m))/(m*np.pi)
+    return 2*(1-np.cos(np.pi*k))/(k*np.pi)
 
-def coef_triang_cos(m):
+def coef_triang_sen(k):
     # onda triangular (serie de Fourier de senos)
-    return 8*np.sin(m*np.pi/2)/ (np.pi*m)**2
+    return 8*np.sin(k*np.pi/2)/ (np.pi*k)**2
 
-def coef_triang_sen(m):
-    # onda triangular (serie de Fourier de senos)
-    return 2 * (np.cos(np.pi*m)-1)/(np.pi*m**2)
+def coef_triang_cos(k):
+    # onda triangular (serie de Fourier de cossenos)
+    return 2 * (np.cos(np.pi*k)-1)/(np.pi*k**2)
 
 
 # %% cria coeficientes da serie de cosenos/senos
 
 # numero de coeficientes a se usar na serie de Fourier de senos+cosenos
-N = 25
+K = 25
 
-assert N <= Nt, ("Numero de coeficientes 'N' eh maior que o numero de amostras no"
+assert K <= Nt, ("Kumero de coeficientes 'K' eh maior que o numero de amostras no"
                  + " tempo 'Nt'- reduza o numero de coeficientes ou aumente a "
                  + "frequencia de amostragem!")
 
 
-A_n = np.zeros(N)
-B_n = np.zeros(N)
+A_k = np.zeros(K)
+B_k = np.zeros(K)
 
 # sinal : ["arbitrario", "serra", "quad", "triang1", "triang2"]
-sinal = "serra"
+sinal = "triang2"
 
 
 match sinal:
     
     # coeficientes arbitrarios
     case "arbitrario":    
-        A_n[1] = 1.2
-        A_n[3] = 0.5
+        A_k[1] = 1.2
+        A_k[3] = 0.5
 
     # Onda dente de serra    
     case "serra":        
-        B_n[1:] = coef_dente_sen(np.arange(1, N))
+        B_k[1:] = coef_dente_sen(np.arange(1, K))
     
     # Onda quadrada
     case "quad":
-        B_n[1:] = coef_quad_sen(np.arange(1, N))
+        B_k[1:] = coef_quad_sen(np.arange(1, K))
 
     # Onda triangular (x entre [-1, 1])
     case "triang1":
-        B_n[1:] = coef_triang_cos(np.arange(1, N))
+        B_k[1:] = coef_triang_sen(np.arange(1, K))
 
     # Onda triangular (x entre 0 e +pi)
     case "triang2":
-        A_n[0] = np.pi/2
-        A_n[1:] = coef_triang_sen(np.arange(1, N))
+        A_k[0] = np.pi/2
+        A_k[1:] = coef_triang_cos(np.arange(1, K))
 
 
 # %% sintetiza sinal no tempo a partir dos coeficientes de Fourier
 
 x = np.zeros(Nt)
-for n in range(N):
-    x += A_n[n]*np.cos(2*np.pi*n*f0*t) + B_n[n]*np.sin(2*np.pi*n*f0*t)
+for n in range(K):
+    x += A_k[n]*np.cos(2*np.pi*n*f0*t) + B_k[n]*np.sin(2*np.pi*n*f0*t)
 
 # calcula coeficientes da serie exponencial (DFT)
 Xf_teorico = np.zeros(Nt, dtype='complex')
-for n in range(N):
+for n in range(K):
     
     # cos(x) = ( exp(1j*x) + exp(-1j*x) )/2
-    Xf_teorico[n] += A_n[n]/2
-    Xf_teorico[-n] += A_n[n]/2
+    Xf_teorico[n] += A_k[n]/2
+    Xf_teorico[-n] += A_k[n]/2
        
     # sin(x) = ( exp(1j*x) - exp(-1j*x) ) / 2j
-    Xf_teorico[n] += B_n[n]/2j
-    Xf_teorico[-n] += -B_n[n]/2j
+    Xf_teorico[n] += B_k[n]/2j
+    Xf_teorico[-n] += -B_k[n]/2j
 
 # --------------------------------------------------------------
 
@@ -135,7 +135,7 @@ plt.plot(t, x)
 plt.grid()
 plt.xlabel("Tempo [s]")
 plt.ylabel("Amplitude")
-plt.title(f"Sinal periodico ({N} coeficientes)")
+plt.title(f"Sinal periodico ({K} coeficientes)")
 
 # %% calcula DFT do sinal
 
@@ -144,8 +144,15 @@ df = fs/Ndft
 
 Xf = np.fft.fft(x)/Ndft
 
-# 'truque' para zerar valores numericamente muito proximos de zero
-Xf[np.abs(Xf)<1e-15] = 0j
+def zera_valores_pequenos(arr, tol=1e-15):
+    """Zera valores reais ou imaginarios muito proximos de zero"""
+    arr.real[np.abs(arr.real) < tol] = 0
+    arr.imag[np.abs(arr.imag) < tol] = 0
+    return arr
+
+Xf = zera_valores_pequenos(Xf)
+Xf_teorico = zera_valores_pequenos(Xf_teorico)
+
 
 f = np.linspace(0, fs-df, Ndft)
 
